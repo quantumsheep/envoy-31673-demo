b315b8cab4adb7820791b83591ed4a8bbf4ec880
Nathanael DEMACON <ndemacon@scaleway.com>
2024-01-09 12:04:41 +0100

    pipe to /dev/null

--- stress.sh
+++ stress.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-echo "GET http://localhost:8080/" | vegeta attack -max-connections=1 -workers=50 -rate=30000 -duration=50000ms -http2 -h2c | vegeta encode
+echo "GET http://localhost:8080/" | vegeta attack -max-connections=1 -workers=50 -rate=30000 -duration=50000ms -http2 -h2c > /dev/null
